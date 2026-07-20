d76f9286ff5a0403e3b0014de95b675c03baf429
MAVI-debug <mazaromeo1@gmail.com>
2026-07-20 15:34:46 -0600

    orden

--- app.py
+++ app.py
@@ -751,7 +751,6 @@ def procesar_sisca():
         JOIN estudiantes e ON r.cui_estudiante = e.cui AND r.usuario_id = e.usuario_id
         WHERE r.codigo_centro = %s AND r.usuario_id = %s
         GROUP BY e.cui, e.usuario_id
-        ORDER BY e.grado, e.seccion, e.nombre_completo
     """, (codigo_centro, uid))
 
     if not registros:
