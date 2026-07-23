2dccafdf7caa8323b4e350517d902bb56e86cc8d
MAVI-debug <mazaromeo1@gmail.com>
2026-07-22 20:36:49 -0600

    Revert "corregido"
    This reverts commit 1f8e63d1c56493bda4e66be58f733e14a7d778a1.

--- sisca_logic.py
+++ sisca_logic.py
@@ -227,14 +227,11 @@ def extraer_alumnos_pdf(ruta_pdf: str) -> list:
             j = i + 1
             while j < len(lineas) and j <= i + 3:
                 prox = lineas[j].strip()
-                if not prox:
+                if prox and not _es_linea_header(prox):
+                    bloque += " " + prox
+                    j += 1
+                else:
                     break
-                if _es_linea_header(prox):
-                    break
-                if _RE_CUI.search(prox) or _RE_CODIGO_PERSONAL.search(prox):
-                    break
-                bloque += " " + prox
-                j += 1
 
             alumno = _extraer_alumno_de_texto(bloque)
             if alumno:
